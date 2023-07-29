ac3147fc105e340d4b1d050e1c849271f479ad43
Manisha1083 <manishakumbhar@gmail.com>
2023-07-29 15:52:37 +0530

    Home Page and App Constants

--- src/main/java/com/sm/RedBus/pages/HomePage.java
+++ src/main/java/com/sm/RedBus/pages/HomePage.java
@@ -1,5 +1,38 @@
 package com.sm.RedBus.pages;
 
-public class HomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+import com.sm.RedBus.utils.AppConstants;
+import com.sm.RedBus.utils.ElementUtil;
+
+public class HomePage {
+	private WebDriver driver;
+	private ElementUtil eleUtil;
+
+	//1. const. of the page class
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		eleUtil = new ElementUtil(this.driver);
+	}
+	
+	//2. by locators:
+		private By logo = By.xpath("//*[@id=\"main_header_new\"]/div/img");
+		private By account = By.xpath("//*[@id=\"account_dd\"]/div[1]/span");
+		private By from = By.id("src");
+		private By to =By.cssSelector("#dest") ;
+		private By search = By.xpath("//*[@id=\"search_button\"]");
+		
+		//3. page actions:
+		public String getHomePageTitle() {
+			return eleUtil.waitForTitleIsAndCapture(AppConstants.HOME_PAGE_TITLE_VALUE, AppConstants.SHORT_DEFAULT_WAIT);
+		}
+		
+		public boolean isLogoExist() {
+			return eleUtil.checkElementIsDisplayed(logo);
+		}
+		
+//		public boolean isMyAccountLinkExist() {
+//			return eleUtil.checkElementIsDisplayed(myAccount);
+//		}
 }
